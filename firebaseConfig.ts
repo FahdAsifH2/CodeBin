8c1e694950bb689fc935d203daae04c9b96ea24b
Fahd Asif <151466690+FahdAsifH2@users.noreply.github.com>
2024-07-14 12:26:10 -0700

    Add files via upload

---
+++ firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig =
+{
+  apiKey: "AIzaSyAgw3TLFppHHUmuO3V2RUhkYNPrAazpanI",
+  authDomain: "codebin-ng-yt-1d387.firebaseapp.com",
+  projectId: "codebin-ng-yt-1d387",
+  storageBucket: "codebin-ng-yt-1d387.appspot.com",
+  messagingSenderId: "518736190734",
+  appId: "1:518736190734:web:a51e8f453a828237f48b5f"
+};
